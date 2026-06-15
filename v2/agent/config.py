46550136678a584873afa1e39b8bf11dbb0f12d7
castledragon <starlight1532@gmail.com>
2026-06-15 22:50:29 +0900

    k8s + pod +docker img push 성공 후 , 배포.

--- v2/agent/config.py
+++ v2/agent/config.py
@@ -1,5 +1,6 @@
 # 클라우드 배포 후 Railway URL로 이 줄만 교체하면 됨
-API_URL = "http://localhost"
+# API_URL = "http://localhost" # 개발환경
+API_URL = "http://a8696e16291a949eabfb2456ef0db4df-212562611.ap-northeast-2.elb.amazonaws.com" # k8s 실제 주소.
 
 # 수집 간격 (분)
 INTERVAL_MINUTES = 5
